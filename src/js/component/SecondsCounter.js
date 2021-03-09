ff2c1346ccde2f86288f4b5c59a9af8d586b2b49
Karla9191 <marietha2014@gmail.com>
2021-03-09 02:57:14 +0000

    HM

---
+++ src/js/component/SecondsCounter.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function SecondsC() {
+	return <p>Hola, KArla</p>;
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,10 +3,13 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+import { SecondsC } from "./SecondsCounter.js";
+
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
+			<SecondsC />
 			<h1>Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
